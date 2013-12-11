f5a666625ce99e06a6d6a51093dbe5b701eb9be8
Silvio Traversaro <pegua1@gmail.com>
2013-12-11 17:51:50 +0900

    Added support for buffering data instead of writing to file at every loop

--- nao_identification_datalogger.py
+++ nao_identification_datalogger.py
@@ -10,6 +10,8 @@ import naoqi
 from naoqi import ALProxy
 
 #This is the list of the names of the device to log
+#to improve the performance of the logging (higher framerate, less jitter) 
+#reduce the number of readed topics
 sensor_to_log_list = [ 'DCM/Time',
                        'DCM/CycleTime',
                        'Device/SubDeviceList/Head/Touch/Middle/Sensor/Value',
@@ -54,7 +56,7 @@ sensor_to_log_list = [ 'DCM/Time',
                        "Device/SubDeviceList/RFoot/FSR/RearLeft/Sensor/Value",
                        "Device/SubDeviceList/RFoot/FSR/RearRight/Sensor/Value"]
                         
-def getdata(NAOIP='nao.local',NAOPORT=9559,PERIOD_IN_SECONDS=0.05):                                        #set name
+def getdata(NAOIP='nao.local',NAOPORT=9559,PERIOD_IN_SECONDS=0.05,MAX_BUFFER_SIZE=20):                                        #set name
     log=ALProxy('ALLogger',NAOIP,NAOPORT)                                #call ALLogger
     frame = ALProxy('ALFileManager',NAOIP,NAOPORT)                        #call ALFileManager
     
@@ -70,22 +72,39 @@ def getdata(NAOIP='nao.local',NAOPORT=9559,PERIOD_IN_SECONDS=0.05):             
     mem = ALProxy('ALMemory',NAOIP,NAOPORT)                            #call ALMemory
     output.write('SampleNumber\t'+('\t'.join(sensor_to_log_list))+'\n')
     output.flush()
+    
+    #List of string used as a buffer to avoid writing to file at each loop
+    #(writing to file it is a very costly (in terms of time) operation)
+    buffer_list = []
+    
     x=0
     log.info('log','loop start')
     while 1:
+    
         start_time = time.time()
+        
+        #Get desired data from ALMemory
         data = mem.getListData(sensor_to_log_list)
-        data.insert(0,x)
-        str_data = map(str,data)
-        temp_data = '\t'.join(str_data)
+        
+        #Adding the row with data (separated by the tab) to the buffer list
+        buffer_list.append(str(x)+'\t'.join(map(str,data))+'\n')
         output.write(temp_data+'\n')
         x += 1
         end_time = time.time()
-        #print('Seconds for getting data and putting in file: '+str(end_time-start_time))
-        #quick hack to avoid busy waiting 
+        
+	#If the buffer reached the max size, write its content to file 
+	if( len(buffer_list) > MAX_BUFFER_SIZE ):
+	    output.write(''.join(buffer_list));
+	    #and then empty the buffer
+	    buffer_list = []
+	
+	#as PERIOD_IN_SECONDS contains the desired logging period
+	#and end_time-start_time contains (approximatly) the time used by the loop
+	#then PERIOD_IN_SECONDS-(end_time-start_time) is the remaining time (if there is any)
         remaining_time = PERIOD_IN_SECONDS-end_time+start_time;
 	if( remaining_time > 0 ):
 	    time.sleep(remaining_time)
+        
         
 def main():
     """ Script to dump values of the sensors related to identifiation on 
